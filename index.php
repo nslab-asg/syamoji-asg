d7605cb03473bf5b30507c5db5d163bb34fa6426
Tom syamoji <uniquequeue@gmail.com>
2014-08-06 23:19:45 +0900

    index.php added 2 textfroms

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
+print <<< EOF
+<html>
+<head>
+<meta charset="utf-8" />
+<title>syamoji</title>
+</head>
+<body>
+    <form method="post">
+      <input type="text" name="posterName" size="30">
+      <input type="text" name="feederContent" size="120">
+    </form>
+</body>
+</html>
+EOF;
 echo "hello feeder world!";
 ?>
