0420b9074be63685ba104e634e1a0385a008aa09
syamoji <uniquequeue@gmail.com>
2014-08-10 16:25:13 +0900

    mongodb insert test. select collection

--- mongo.php
+++ mongo.php
@@ -2,9 +2,9 @@
 				$mongo = new MongoClient(getenv('MONGOHQ_URL'));
 				$doc = $mongo->app28134254->feedCollection;
 
-				$collection = $mongo->selectCollection('feedCollection');
+
 				$a = array('name' => 'tkj');
-				$collection->insert($a);
+				$doc->insert($a);
 
 				$feed = $doc->findOne(array('name' => 'tkj'));
 				print_r($feed);
