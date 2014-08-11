17686ede169e845536dbe775a14ab828ac410d02
syamoji <uniquequeue@gmail.com>
2014-08-11 15:27:48 +0900

    add mongo.php comment

--- mongo.php
+++ mongo.php
@@ -1,16 +1,19 @@
 <?php
+				// first, run below command
+				// heroku config:set MONGOHQ_URL=...
+
 				// db connection
 				$mongo = new MongoClient(getenv('MONGOHQ_URL'));
 				// select collection
 				$doc = $mongo->app28134254->feedCollection;
 
 				// query string
-				$a = array('name' => 'tkj');
+				$a = array('name' => 'search name');
 				// insert
 				$doc->insert($a);
 
 				// find
-				$feed = $doc->findOne(array('name' => 'tkj'));
+				$feed = $doc->findOne(array('name' => 'search name'));
 
 				// log
 				print_r($feed);
