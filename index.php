1b80a2c35de845fb3b4d1099f1fea4ce0aa0f203
syamoji <uniquequeue@gmail.com>
2014-08-19 23:48:40 +0900

    after $.post .text to .html

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ print <<< EOF
 <body>
     <input type="text" id="posterName" size="20">
     <input type="text" id="feederContent" size="80">
-    <button id="postButton" onClick="$.post('mongo_insert.php',{feederName: $('#posterName').val(), feederPost: $('#feederContent').val()}).done(function(){alert('done!'); $.get('mongo_get.php', function(data){ $('#feedPage').text(data)});});">投稿</button>
+    <button id="postButton" onClick="$.post('mongo_insert.php',{feederName: $('#posterName').val(), feederPost: $('#feederContent').val()}).done(function(){alert('done!'); $.get('mongo_get.php', function(data){ $('#feedPage').html(data)});});">投稿</button>
     <div id="feedPage"></div>
 </body>
 </html>
