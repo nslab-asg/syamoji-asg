bcc711ea14220d32829ecbe202a02b0fac17ad39
Tom syamoji <uniquequeue@gmail.com>
2014-08-21 00:36:10 +0900

    README.md written. and ready function is set

--- index.php
+++ index.php
@@ -4,12 +4,17 @@ print <<< EOF
 <head>
 <meta charset="utf-8" />
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script>
+    $(document).ready(function(){
+        $.get('mongo_get.php', function(data){ $('#feedPage').html(data)});
+    });
+</script>
 <title>syamoji</title>
 </head>
 <body>
     <input type="text" id="posterName" size="20">
     <input type="text" id="feederContent" size="80">
-    <button id="postButton" onClick="$.post('mongo_insert.php',{feederName: $('#posterName').val(), feederPost: $('#feederContent').val()}).done(function(){alert('done!'); $.get('mongo_get.php', function(data){ $('#feedPage').html(data)});});">投稿</button>
+    <button id="postButton" onClick="$.post('mongo_insert.php',{feederName: $('#posterName').val(), feederPost: $('#feederContent').val()}).done(function(){alert('done!'); $('#feederContent').val(''); $.get('mongo_get.php', function(data){ $('#feedPage').html(data)});});">投稿</button>
     <div id="feedPage"></div>
 </body>
 </html>
