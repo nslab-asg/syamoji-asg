25f5d90eeb4141e975409a82a03044e01202956d
syamoji <uniquequeue@gmail.com>
2014-08-27 01:02:21 +0900

    del method php fix12

--- mongo_del.php
+++ mongo_del.php
@@ -6,7 +6,7 @@ $doc = $mongo->app28134254->feedCollection;
 
 // find
 $feed = $doc->find(array('_id' => new \MongoId('53e8de53964c133e008b4567')));//false))->sort(array('_id' => -1));
-
+print_r($feed);
 if (isset($_GET['delId']))
 {
     foreach($feed as $id => $obj) {   
