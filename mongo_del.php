9874f19a046abbf658a03dc329570722e42f33ca
syamoji <uniquequeue@gmail.com>
2014-08-27 23:45:36 +0900

    del method php fix15

--- mongo_del.php
+++ mongo_del.php
@@ -5,15 +5,16 @@ $mongo = new MongoClient(getenv('MONGOHQ_URL'));
 $doc = $mongo->app28134254->feedCollection;
 
 // find
-$feed = $doc->find(array('_id' => new MongoId('53e8de53964c133e008b4567')));//false))->sort(array('_id' => -1));
+//$feed = $doc->find(array('_id' => new MongoId('53e8de53964c133e008b4567')));//false))->sort(array('_id' => -1));
 print_r($feed);
 if (isset($_GET['delId']))
 {
-    foreach($feed as $id => $obj) {   
-        $gg = $doc->update(array('_id' => $obj[_id]), array('$set' => array('deleteFlag' => true)));
-        print_r($gg);
-        print($_GET['delId']);
-    }   
+    $doc->update(array('_id' => new MongoId($_GET['delId'])), array('$set' => array('deleteFlag' => true)));
+    //foreach($feed as $id => $obj) {   
+    //  $gg = $doc->update(array('_id' => $obj[_id]), array('$set' => array('deleteFlag' => true)));
+    //  print_r($gg);
+    //  print($_GET['delId']);
+    //}   
 } else {
     return;
 }
