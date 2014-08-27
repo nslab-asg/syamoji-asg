7c8cb340deb80cec57193b0baf9ec6f4c84c06b9
syamoji <uniquequeue@gmail.com>
2014-08-27 23:31:39 +0900

    del method php fix13

--- mongo_del.php
+++ mongo_del.php
@@ -5,7 +5,7 @@ $mongo = new MongoClient(getenv('MONGOHQ_URL'));
 $doc = $mongo->app28134254->feedCollection;
 
 // find
-$feed = $doc->find(array('_id' => new \MongoId('53e8de53964c133e008b4567')));//false))->sort(array('_id' => -1));
+$feed = $doc->find(array('_id' => new MongoId('53e8de53964c133e008b4567')));//false))->sort(array('_id' => -1));
 print_r($feed);
 if (isset($_GET['delId']))
 {
